b6715178241a4de4ca7bf440b77fd0a621818da1
Jeff Casimir <jeff@casimircreative.com>
2013-08-09 20:35:16 -0700

    Project foundation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookhub::Application.config.secret_token = '4c0061756550ed8bab6025b536a06b64ba083b6489c1464bdeabee02bbe075258ad8c557837de4c287928c22031c05c99c87d64465af9442ddc96025829a8551'
